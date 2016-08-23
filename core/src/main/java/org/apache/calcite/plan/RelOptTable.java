vkorukanti/optiq

Add new method to ViewExpander interface to allow passing SchemaRoot.

core/src/main/java/org/eigenbase/relopt/RelOptTable.java
@@ -24,6 +24,8 @@ import org.eigenbase.reltype.*;
 
 import net.hydromatic.linq4j.expressions.Expression;
 
+import net.hydromatic.optiq.SchemaPlus;
+
 /**
  * Represents a relational dataset in a {@link RelOptSchema}. It has methods to
  * describe and implement itself.
@@ -100,6 +102,12 @@ public interface RelOptTable {
     RelNode expandView(
         RelDataType rowType,
         String queryString,
+        List<String> schemaPath);
+
+    RelNode expandView(
+        RelDataType rowType,
+        String queryString,
+        SchemaPlus rootSchema,
         List<String> schemaPath);
   }
 

core/src/main/java/net/hydromatic/optiq/prepare/PlannerImpl.java
@@ -199,6 +199,31 @@ public class PlannerImpl implements Planner {
   public class ViewExpanderImpl implements ViewExpander {
     public RelNode expandView(RelDataType rowType, String queryString,
         List<String> schemaPath) {
+      final OptiqCatalogReader catalogReader =
+          createCatalogReader().withSchemaPath(schemaPath);
+
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    public RelNode expandView(
+        RelDataType rowType,
+        String queryString,
+        SchemaPlus rootSchema,
+        List<String> schemaPath) {
+
+      // View may have different schema path than current connection.
+      final OptiqCatalogReader catalogReader = new OptiqCatalogReader(
+          OptiqSchema.from(rootSchema),
+          parserConfig.caseSensitive(),
+          OptiqSchema.from(defaultSchema).path(null),
+          typeFactory).withSchemaPath(schemaPath);
+
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    private RelNode expandViewHelper(
+        String queryString,
+        OptiqCatalogReader catalogReader) {
       SqlParser parser = SqlParser.create(parserFactory, queryString,
           parserConfig);
       SqlNode sqlNode;
@@ -208,8 +233,6 @@ public class PlannerImpl implements Planner {
         throw new RuntimeException("parse failed", e);
       }
 
-      final OptiqCatalogReader catalogReader =
-          createCatalogReader().withSchemaPath(schemaPath);
       SqlValidator validator = createSqlValidator(catalogReader);
       validator.setIdentifierExpansion(true);
       SqlNode validatedSqlNode = validator.validate(sqlNode);

core/src/main/java/net/hydromatic/optiq/prepare/Prepare.java
@@ -18,6 +18,7 @@
 package net.hydromatic.optiq.prepare;
 
 import net.hydromatic.optiq.DataContext;
+import net.hydromatic.optiq.SchemaPlus;
 import net.hydromatic.optiq.impl.StarTable;
 import net.hydromatic.optiq.jdbc.OptiqPrepare;
 import net.hydromatic.optiq.jdbc.OptiqSchema;
@@ -374,6 +375,25 @@ public abstract class Prepare {
   public RelNode expandView(
       RelDataType rowType,
       String queryString,
+      List<String> schemaPath) {
+    throw new UnsupportedOperationException();
+  }
+
+
+  /**
+   * Returns a relational expression which is to be substituted for an access
+   * to a SQL view.
+   *
+   * @param rowType Row type of the view
+   * @param queryString Body of the view
+   * @param rootSchema Root schema of the schema tree
+   * @param schemaPath List of schema names wherein to find referenced tables
+   * @return Relational expression
+   */
+  public RelNode expandView(
+      RelDataType rowType,
+      String queryString,
+      SchemaPlus rootSchema,
       List<String> schemaPath) {
     throw new UnsupportedOperationException();
   }

rkulyk/incubator-calcite

Conflicts:
	core/src/main/java/org/apache/calcite/plan/RelOptTable.java
	core/src/main/java/org/apache/calcite/plan/RelOptUtil.java
	core/src/main/java/org/apache/calcite/prepare/PlannerImpl.java
	core/src/main/java/org/apache/calcite/sql2rel/SqlToRelConverter.java

core/src/main/java/org/apache/calcite/plan/RelOptTable.java
@@ -24,6 +24,7 @@ import org.apache.calcite.rel.RelRoot;
 import org.apache.calcite.rel.metadata.RelMetadataQuery;
 import org.apache.calcite.rel.type.RelDataType;
 import org.apache.calcite.rel.type.RelDataTypeField;
+import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.util.ImmutableBitSet;
 
 import java.util.List;
@@ -125,6 +126,12 @@ public interface RelOptTable {
      */
     RelRoot expandView(RelDataType rowType, String queryString,
         List<String> schemaPath, List<String> viewPath);
+
+    RelRoot expandView(
+        RelDataType rowType,
+        String queryString,
+        SchemaPlus rootSchema,
+        List<String> schemaPath);
   }
 
   /** Contains the context needed to convert a a table into a relational

core/src/main/java/org/apache/calcite/prepare/PlannerImpl.java
@@ -235,7 +235,26 @@ public class PlannerImpl implements Planner {
    * interface for {@link org.apache.calcite.tools.Planner}. */
   public class ViewExpanderImpl implements ViewExpander {
     @Override public RelRoot expandView(RelDataType rowType, String queryString,
-      List<String> schemaPath, List<String> viewPath) {
+        List<String> schemaPath, List<String> viewPath) {
+      final CalciteCatalogReader catalogReader =
+          createCatalogReader().withSchemaPath(schemaPath);
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    @Override public RelRoot expandView(RelDataType rowType, String queryString,
+        SchemaPlus rootSchema, List<String> schemaPath) {
+      // View may have different schema path than current connection.
+      final CalciteCatalogReader catalogReader = new CalciteCatalogReader(
+          CalciteSchema.from(rootSchema),
+          parserConfig.caseSensitive(),
+          CalciteSchema.from(defaultSchema).path(null),
+          typeFactory).withSchemaPath(schemaPath);
+      return expandViewHelper(queryString, catalogReader);
+    }
+
+    private RelRoot expandViewHelper(
+        String queryString,
+        CalciteCatalogReader catalogReader) {
       SqlParser parser = SqlParser.create(queryString, parserConfig);
       SqlNode sqlNode;
       try {
@@ -245,8 +264,6 @@ public class PlannerImpl implements Planner {
       }
 
       final SqlConformance conformance = conformance();
-      final CalciteCatalogReader catalogReader =
-          createCatalogReader().withSchemaPath(schemaPath);
       final SqlValidator validator =
           new CalciteSqlValidator(operatorTable, catalogReader, typeFactory,
               conformance);

core/src/main/java/org/apache/calcite/prepare/Prepare.java
@@ -38,6 +38,7 @@ import org.apache.calcite.rex.RexExecutorImpl;
 import org.apache.calcite.runtime.Bindable;
 import org.apache.calcite.runtime.Hook;
 import org.apache.calcite.runtime.Typed;
+import org.apache.calcite.schema.SchemaPlus;
 import org.apache.calcite.schema.impl.StarTable;
 import org.apache.calcite.sql.SqlExplain;
 import org.apache.calcite.sql.SqlExplainLevel;
@@ -368,6 +369,25 @@ public abstract class Prepare {
 
   public RelRoot expandView(RelDataType rowType, String queryString,
       List<String> schemaPath, List<String> viewPath) {
+    throw new UnsupportedOperationException();
+  }
+
+
+  /**
+   * Returns a relational expression which is to be substituted for an access
+   * to a SQL view.
+   *
+   * @param rowType Row type of the view
+   * @param queryString Body of the view
+   * @param rootSchema Root schema of the schema tree
+   * @param schemaPath List of schema names wherein to find referenced tables
+   * @return Relational expression
+   */
+  public RelRoot expandView(
+      RelDataType rowType,
+      String queryString,
+      SchemaPlus rootSchema,
+      List<String> schemaPath) {
     throw new UnsupportedOperationException();
   }
 